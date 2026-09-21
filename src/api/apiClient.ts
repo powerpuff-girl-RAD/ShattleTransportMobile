3564735876295f0274ea42a0366378fd4ea12286
N-ach-Shalani <shalani@n-ach.com>
2026-09-21 19:42:06 +0530

    commit 3

---
+++ src/api/apiClient.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const API_BASE_URL = process.env.EXPO_PUBLIC_API_URL;
+
+export const apiClient = axios.create({
+    baseURL: API_BASE_URL,
+    timeout: 10000,
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+export default apiClient;
